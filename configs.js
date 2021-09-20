02907a460935294fe37211c18d6d57decdd62c7e
PeterLou645 <47363597+PeterLou645@users.noreply.github.com>
2021-09-20 14:23:12 -0400

    first commit

---
+++ configs.js
@@ -0,0 +1,17 @@
+module.exports = {
+    // twitter api configs
+    consumer_key:     '', 
+    consumer_secret:  '',
+    access_token:         '',
+    access_token_secret:  '',
+
+    // open weather api configs
+    apiLink: 'http://api.openweathermap.org/data/2.5/weather?q=',
+    city: 'Montreal',
+    country: 'CA',
+    appID: ['&appid=',''],
+    units: ['&units=','metric'],
+  
+    // twitter username
+    username: '@mtlweatherbot'
+  }
